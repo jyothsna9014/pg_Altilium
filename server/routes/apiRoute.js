f4d940a54362ba0f33635fa505ef320df56ea523
jyothsna <jyothsna9014@gmail.com>
2019-11-18 17:15:35 +0530

    dashboardmqtt

--- server/routes/apiRoute.js
+++ server/routes/apiRoute.js
@@ -3,11 +3,7 @@ var router = express.Router();
 const logfixturescontroller = require('../controllers/logfixturescontroller');
 const fixtureDetailsController=require('../controllers/fixtureDetailsController');
 const fixturesController=require('../controllers/fixturesController');
-const batterystatuscontroller=require('../controllers/batterystatuscontroller');
-const LuminariesOpModeONController=require('../controllers/LuminariesOpModeONController')
-const LuminariesOpModeOFFController=require('../controllers/LuminariesOpModeOFFController')
-const LitalityNodesController=require('../controllers/LitalityNodesController')
-const ONGridONBatteryController=require('../controllers/ONGridONBatteryController')
+
 const dashBoardController=require('../controllers/dashBoardController.js')
 /* GET home page. */
 router.get('/log', logfixturescontroller.getlog);       
@@ -17,13 +13,7 @@ router.get('/log',fixturesController.getfixtures);
 router.get('/fixturedetails',fixtureDetailsController.getfixturedetails);
 router.get('/fixtures',fixturesController.getfixtures);
 
-router.get('/batterystatus',batterystatuscontroller.getbatterystatus);
-router.get('/opmodeON',LuminariesOpModeONController.getopmodeON);
-router.get('/opmodeOFF',LuminariesOpModeOFFController.getopmodeOFF);
-router.get('/litalitynodes',LitalityNodesController.getLitalityNodes);
 
-router.get('/ONGrid',ONGridONBatteryController.getONGrid);
-router.get('/ONBattery',ONGridONBatteryController.getONBattery);
 router.get('/dashboard',dashBoardController.getdashboard);
 
 module.exports = router;
